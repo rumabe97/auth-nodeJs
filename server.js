22b4df67ce44eaa7e78cff5e2c29ec2c90c97547
rumabe97 <rumabe97@gmail.com>
2022-08-02 14:19:51 +0200

    Start project

---
+++ server.js
@@ -0,0 +1,18 @@
+// require express
+const express = require("express");
+
+//create an app using express constructor
+const auth = express();
+
+// declare your port
+const port = 5000;
+
+// require routes from the routes.js file
+// const routes = require("./api/routes");
+// set the route for our application by passing the app to the routes object
+routes(auth);
+
+// call the listen method on the app
+auth.listen(port, ()=>{
+    console.log("Server is running is port: " + port);
+});
