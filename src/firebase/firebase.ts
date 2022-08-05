3889760f10e19742f7f9d39e275abb8ad399d5e0
rumabe97 <rumabe97@gmail.com>
2022-08-05 11:55:44 +0200

    Add swagger

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import {SignupController} from "./adapter/in/controllers/Signup-controller";
 import {initializeApp, cert} from 'firebase-admin/app';
 
-const GOOGLE_APPLICATION_CREDENTIALS = '/home/ruben/Escritorio/githubProyects/auth-nodeJs//serviceAccountKey.json';
+const GOOGLE_APPLICATION_CREDENTIALS = '/home/ruben/Escritorio/githubProyects/auth-nodeJs/serviceAccountKey.json';
 
 initializeApp({
     credential: cert(GOOGLE_APPLICATION_CREDENTIALS),
@@ -12,7 +12,6 @@ initializeApp({
 
 const router = express.Router();
 const signupController = new SignupController();
-
 
 router.post('/signup', async (req, res) => {
     const response = await signupController.signup({...req?.body});
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('../swagger.json');
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 import express from 'express';
 import {firebaseRoutes} from "./firebase/firebase";
 import bodyParser from "body-parser";
+import swaggerUi from "swagger-ui-express";
+import * as fs from "fs";
+
+const swaggerDocument = '/home/ruben/Escritorio/githubProyects/auth-nodeJs/swagger.json';
+const swaggerData = fs.readFileSync(swaggerDocument, 'utf8');
 
 const app = express();
 app.use(express.json())
@@ -13,9 +13,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}));
 
-const PORT = 3000;
-app.use('', firebaseRoutes);
-
+const PORT = 8080;
+app.use('/api/v0/auth', firebaseRoutes);
+app.use('/api/v0/auth', swaggerUi.serve, swaggerUi.setup(JSON.parse(swaggerData), null, null, null));
+app.use("*", (req, res) => {
+    res.send("Make sure url is correct!");
+})
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
 })
