2592462e69a8d0e720a7d95d05ac25498ea81892
rumabe97 <rumabe97@gmail.com>
2022-08-09 14:42:34 +0200

    Change all logic to controller

--- src/firebase/adapter/in/controllers/Signup-controller.ts
+++ src/firebase/adapter/in/controllers/Signup-controller.ts
@@ -1,14 +1,33 @@
 import {SignupService} from "../../../application/service/Signup-service";
 import {SignupInputDto} from "../dtos/SignupInputDto";
+import express from "express";
+import {CODE_OK} from "../../../../shared/enums/Errors";
+import {clean} from "../../../../shared/objectUtils/Clean";
+import {SignupOutputDto} from "../../out/dtos/SignupOutputDto";
+import {ResponseService} from "../../../../shared/errors/ErrorService";
 
 export class SignupController {
     private signupService: SignupService;
+    private router:any;
 
     constructor() {
         this.signupService = new SignupService();
+        this.router = express.Router();
     }
 
-    public signup(user: SignupInputDto): any {
-        return this.signupService.signup(user);
+    public signup(): any {
+        return this.router.post("/", async (req, res) => {
+            let status = 'Success Request', statusCode = CODE_OK, message = '';
+
+            const signupInputDto: SignupInputDto = new SignupInputDto(req.body);
+            const data:any = await this.signupService.signup(clean(signupInputDto));
+            if (data.err) {
+                statusCode = data.err.code;
+                message = data.err.message;
+                status = 'Failure Request'
+            }
+            const resp = statusCode === CODE_OK ? new SignupOutputDto(data) : ResponseService(status, statusCode, message, data.err ? null : data);
+            return res.status(statusCode).send(resp);
+        })
     }
 }
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,12 +1,6 @@
 import express from 'express';
 import {SignupController} from "./adapter/in/controllers/Signup-controller";
 import {initializeApp, cert} from 'firebase-admin/app';
-import {ResponseService} from "../shared/errors/ErrorService";
-import {CODE_OK} from "../shared/enums/Errors";
-import {SignupInputDto} from "./adapter/in/dtos/SignupInputDto";
-import {SignupOutputDto} from "./adapter/out/dtos/SignupOutputDto";
-import {SharedResponse} from "../shared/SharedResponse";
-import {clean} from "../shared/objectUtils/Clean";
 
 const GOOGLE_APPLICATION_CREDENTIALS = '/home/ruben/Escritorio/githubProyects/auth-nodeJs/serviceAccountKey.json';
 
@@ -19,19 +13,5 @@ initializeApp({
 const router = express.Router();
 const signupController = new SignupController();
 
-// @ts-ignore
-router.post('/signup', async (req, res): Promise<any, number> => {
-    let status = 'Success Request', statusCode = CODE_OK, message = '';
-
-    const signupInputDto: SignupInputDto = new SignupInputDto(req.body);
-    const data = await signupController.signup(clean(signupInputDto));
-    if (data.err) {
-        statusCode = data.err.code;
-        message = data.err.message;
-        status = 'Failure Request'
-    }
-    const resp = statusCode === CODE_OK ? new SignupOutputDto(data) : ResponseService(status, statusCode, message, data.err ? null : data);
-    return res.status(statusCode).send(resp);
-});
-
+router.use("/signup", signupController.signup())
 export const firebaseRoutes = router;
